360580f751d0b10744330548b5f0fce29b20d25c
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-04-19 18:16:08 +0200

    feat: add dir and dbfilename cli commands

--- src/config.rs
+++ src/config.rs
@@ -2,10 +2,16 @@ use clap::{command, Parser};
 
 use crate::state::MasterConfig;
 
+pub struct DatabaseConfig {
+    pub dir: String,
+    pub dbfilename: String,
+}
+
 pub struct Config {
     pub master_config: Option<MasterConfig>,
     pub replicaof: bool,
     pub port: u16,
+    pub db_config: Option<DatabaseConfig>,
 }
 
 #[derive(Parser, Debug)]
@@ -19,6 +25,14 @@ struct Args {
     /// --replicaof <MASTER_HOST> <MASTER_PORT>
     #[clap(short, long, number_of_values = 2)]
     replicaof: Option<Vec<String>>,
+
+    /// The path to the directory where the RDB file is stored
+    #[clap(long)]
+    dir: Option<String>,
+
+    /// The name of the RDB file (example: rdbfile)
+    #[clap(long)]
+    dbfilename: Option<String>,
 }
 
 pub fn load_config() -> anyhow::Result<Config> {
@@ -31,9 +45,16 @@ pub fn load_config() -> anyhow::Result<Config> {
         None
     };
 
+    let db_config = if let (Some(dir), Some(dbfilename)) = (args.dir, args.dbfilename) {
+        Some(DatabaseConfig { dir, dbfilename })
+    } else {
+        None
+    };
+
     Ok(Config {
         master_config,
         replicaof: args.replicaof.is_some(),
         port: args.port,
+        db_config,
     })
 }
