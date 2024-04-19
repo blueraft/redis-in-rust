24a117418a73e640f7b18b3160d09df033ba1d1e
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-04-19 17:56:56 +0200

    refactor: use clap for cli

--- src/config.rs
+++ src/config.rs
@@ -1,44 +1,31 @@
-use std::env;
+use clap::{command, Parser};
 
 use crate::state::MasterConfig;
 
 pub struct Config {
     pub master_config: Option<MasterConfig>,
     pub replicaof: bool,
-    pub port: String,
+    pub port: u16,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Port of the redis server
+    #[arg(short, long, default_value_t = 6379)]
+    port: u16,
+
+    /// a Redis server assumes the "master" role. When the --replicaof flag is passed, the server assumes the "slave" role instead
+    /// --replicaof <MASTER_HOST> <MASTER_PORT>
+    #[clap(short, long, number_of_values = 2)]
+    replicaof: Option<Vec<String>>,
 }
 
 pub fn load_config() -> anyhow::Result<Config> {
-    let mut port = None;
-    let mut replicaof = false;
-    let mut master_host = None;
-    let mut master_port = None;
-    let args: Vec<String> = env::args().collect();
-
-    for i in 1..args.len() {
-        match args[i].as_str() {
-            "--port" | "-p" => {
-                if i + 1 < args.len() {
-                    port = Some(args[i + 1].clone());
-                }
-            }
-            "--replicaof" => {
-                if i + 2 < args.len() {
-                    master_host = Some(args[i + 1].clone());
-                    master_port = Some(args[i + 2].parse().clone()?);
-                }
-                replicaof = true;
-            }
-
-            _ => (),
-        }
-    }
-    let port = match port {
-        Some(p) => p,
-        None => "6379".to_string(),
-    };
-
-    let master_config = if let (Some(host), Some(port)) = (master_host, master_port) {
+    let args = Args::parse();
+    let master_config = if let Some(replicaof) = &args.replicaof {
+        let host = replicaof.first().unwrap().to_owned();
+        let port: usize = replicaof.last().unwrap().parse()?;
         Some(MasterConfig { host, port })
     } else {
         None
@@ -46,7 +33,7 @@ pub fn load_config() -> anyhow::Result<Config> {
 
     Ok(Config {
         master_config,
-        replicaof,
-        port,
+        replicaof: args.replicaof.is_some(),
+        port: args.port,
     })
 }
